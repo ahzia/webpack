b9d1a26e942c3e44ca6c10b64da929baa0223134
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-07-20 19:13:55 +0430

    Setup local dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
